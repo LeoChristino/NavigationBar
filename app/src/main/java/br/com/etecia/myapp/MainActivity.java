5c34db056e5e512161c3eac7238d59a0c6ed8c44
leochristino <leo.chrissantos28@gmail.com>
2024-05-23 21:09:28 -0300

    colocando funções

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -11,7 +11,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView bottomNavView
+    BottomNavigationView bottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,37 +23,20 @@ public class MainActivity extends AppCompatActivity {
         bottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.mNews:
-                        Toast.makeText(getApplicationContext(),
-                                "Cliquei em News", Toast.LENGTH_SHORT).show();
-                        ;
-                        break;
-
-                       switch (item.getItemId()) {
-                        case R.id.mGlobal:
-                            Toast.makeText(getApplicationContext(),
-                                    "Cliquei em Global", Toast.LENGTH_SHORT).show();
-                            ;
-                            break;
-
-                        switch (item.getItemId()) {
-                            case R.id.mForYou:
-                                Toast.makeText(getApplicationContext(),
-                                        "Cliquei em For You", Toast.LENGTH_SHORT).show();
-                                ;
-                                break;
-
-                            switch (item.getItemId()) {
-                                case R.id.mTrendings:
-                                    Toast.makeText(getApplicationContext(),
-                                            "Cliquei em Trendings", Toast.LENGTH_SHORT).show();
-                                    ;
-                                    break;
-
-                            }
-                            return false;
-                        }
-                    });
+                if (item.getItemId() == R.id.mNews) {
+                    Toast.makeText(getApplicationContext(), "Cliquei em News", Toast.LENGTH_SHORT).show();
                 }
+                if (item.getItemId() == R.id.mGlobal) {
+                    Toast.makeText(getApplicationContext(), "Cliquei em Global", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mForYou) {
+                    Toast.makeText(getApplicationContext(), "Cliquei em For You", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mTrendings) {
+                    Toast.makeText(getApplicationContext(), "Cliquei em Trendings", Toast.LENGTH_SHORT).show();
+                }
+                return false;
             }
+        });
+    }
+}
